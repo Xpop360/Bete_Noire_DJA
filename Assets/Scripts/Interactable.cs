503d431e08642e6d6f5710db9c530cffc6ce8cdf
Andreia <andreia.fer.mendes@gmail.com>
2018-04-30 17:27:43 +0100

    Starting inventory

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius;
+    public GameObject player;
+    [HideInInspector]
+    public bool inRadius;
+    float distance;
+
+    void Start()
+    {
+        radius = 0.5f;
+        inRadius = false;
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance < radius)
+        {
+            inRadius = true;
+        }
+        else
+        {
+            inRadius = false;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
