e5abcae1666d829d16d8c9bdb8c23e2ae2002730
Xpop360 <barreiro.pedro7@gmail.com>
2018-04-30 15:59:27 +0100

    Item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory Item")]
+public class Item : ScriptableObject {
+
+    [Tooltip("Name used in inventory list")]
+    public string name;
+
+    [Tooltip("Sprite used in inventory list")]
+    public Sprite icon;
+
+    [Tooltip("3D Game Object used in scene")]
+    public GameObject model;
+
+    [Tooltip("Max number of objects player can carry")]
+    public int maxCount;
+}
