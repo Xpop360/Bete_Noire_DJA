e00624394e3248b48d03d4162c71059327efa42a
Andreia <andreia.fer.mendes@gmail.com>
2018-05-17 15:56:55 +0100

    PuzzleAdded
    666 to open

---
+++ Assets/Scripts/PuzzleUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleUIController : MonoBehaviour {
+
+    Interactable inte;
+    GameObject UI;
+
+    void Start () {
+        inte = GetComponent<Interactable>();
+        UI = GameObject.FindGameObjectWithTag("PuzzleUI");
+        UI.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(inte.inRadius && Input.GetKeyDown(KeyCode.E)) //inRadius pressing E once
+        {
+            UI.SetActive(!UI.activeSelf);
+            if(UI.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+	}
+}
