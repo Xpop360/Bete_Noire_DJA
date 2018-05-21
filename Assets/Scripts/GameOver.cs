04763616591816a45ba3afee25db79a0b4de996e
Xpop360 <barreiro.pedro7@gmail.com>
2018-05-21 15:59:09 +0100

    Game Over

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject gameOver;
+    public Animator animator;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            gameObject.SetActive(true);
+            animator.SetTrigger("gameover");
+        }
+	}
+}
