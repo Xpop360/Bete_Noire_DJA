76071edf8a5372507f8e78eda5a2c0fd2abbac47
Andreia <andreia.fer.mendes@gmail.com>
2018-05-21 14:53:31 +0100

    sghbsfh

--- Assets/Scripts/PuzzleUIController.cs
+++ Assets/Scripts/PuzzleUIController.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PuzzleUIController : MonoBehaviour {
+public class PuzzleUIController : MonoBehaviour
+{
 
     Interactable inte;
     GameObject UI;
 
-    void Start () {
+    void Start()
+    {
         inte = GetComponent<Interactable>();
         UI = GameObject.FindGameObjectWithTag("PuzzleUI");
         UI.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(inte.inRadius && Input.GetKeyDown(KeyCode.E)) //inRadius pressing E once
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((inte.inRadius && Input.GetKeyDown(KeyCode.E))) //inRadius pressing E once
         {
             UI.SetActive(!UI.activeSelf);
-            if(UI.activeSelf)
+            if (UI.activeSelf)
             {
                 Time.timeScale = 0f;
             }
@@ -28,5 +31,9 @@ public class PuzzleUIController : MonoBehaviour {
                 Time.timeScale = 1f;
             }
         }
-	}
+        if(PauseMenu.gamePause)
+        {
+            UI.SetActive(false);
+        }
+    }
 }
