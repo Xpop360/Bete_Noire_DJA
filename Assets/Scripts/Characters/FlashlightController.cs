36377c561552017f778e49271b407288f4f4db13
Andreia <andreia.fer.mendes@gmail.com>
2018-06-05 18:44:53 +0100

    flashlight
    F for toogle

---
+++ Assets/Scripts/Characters/FlashlightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour {
+
+    Light flashlight;
+
+	// Use this for initialization
+	void Start () {
+        flashlight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+	}
+}
